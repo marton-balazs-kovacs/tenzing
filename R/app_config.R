b17ebe3e3ab23b757e5ff23edbb113bae31d41b2
Marton Kovacs <marton.balazs.kovacs@gmail.com>
2020-06-23 16:06:41 +0200

    R CMD check did not find in Dependencies, default golem function, we do not use it currenlty so I took it out

--- R/app_config.R
+++ R/app_config.R
@@ -15,7 +15,7 @@ app_sys <- function(...){
 #' @param config R_CONFIG_ACTIVE value. 
 #' @param use_parent Logical, scan the parent directory for config file.
 #'     
-#' @importFrom config get
+# #' @importFrom config get
 #' 
 #' @noRd
 get_golem_config <- function(
