518a4fff22f91af54ec709660aedf50030648627
Marton Kovacs <marton.balazs.kovacs@gmail.com>
2020-08-17 14:38:25 +0200

    I decided to go with the two separate upload buttons so users can use the url and the local path at the same time. the lastly uploaded infosheet will used in the app.

--- R/app_ui.R
+++ R/app_ui.R
@@ -18,27 +18,26 @@ app_ui <- function() {
                wellPanel(
                  class = "main-steps-panel",
                  h3("1. Create your infosheet", class = "main-steps-title"),
-                 tags$p("1. Copy and fill out this ", style = "display: inline;"),
-                 tags$a(href="https://docs.google.com/spreadsheets/d/1Gl0cwqN_nTsdFH9yhSvi9NypBfDCEhViGq4A3MnBrG8/edit?usp=sharing",
+                 tags$p("First copy and then fill out this ", style = "display: inline;"),
+                 tags$a(href = "https://docs.google.com/spreadsheets/d/1Gl0cwqN_nTsdFH9yhSvi9NypBfDCEhViGq4A3MnBrG8/edit?usp=sharing",
                         "infosheet template",
                         target="_blank",
-                        style = "display: inline;"),
-               actionButton("debug",
-                            label = "debug on R"),
-               ),
-        
+                        style = "display: inline;")
+                 ),
                wellPanel(
                  class = "main-steps-panel",
-                 h3("2. Load your infosheet:", class = "main-steps-title"),
+                 h3("2. Load your infosheet", class = "main-steps-title"),
                  mod_read_spreadsheet_ui("read_spreadsheet_ui_1"),
-                 mod_show_spreadsheet_ui("show_spreadsheet_ui_1")),
+                 mod_show_spreadsheet_ui("show_spreadsheet_ui_1")
+                 ),
                wellPanel(
                  class = "main-steps-panel",
                  h3("3. Download the output", class = "main-steps-title"),
                  mod_human_readable_report_ui("human_readable_report_ui_1"),
                  mod_contribs_affiliation_page_ui("contribs_affiliation_page_ui_1"),
                  mod_xml_report_ui("xml_report_ui_1"),
-                 mod_show_yaml_ui("show_yaml_ui_1"))
+                 mod_show_yaml_ui("show_yaml_ui_1")
+                 )
                ),
         column(6,
                wellPanel(
--- R/mod_read_spreadsheet.R
+++ R/mod_read_spreadsheet.R
@@ -14,29 +14,42 @@
 #' @export 
 #' @importFrom shiny NS tagList 
 mod_read_spreadsheet_ui <- function(id){
-  ns <- NS(id)
+
   tagList(
     h5("Choose the spreadsheet on your computer", class = "main-steps-title"),
-    fileInput(ns("file"),
-              label = NULL,
-              accept = c(
-                '.csv',
-                '.tsv',
-                '.xlsx'),
-              multiple = FALSE),
-    h5("or copy the url of a shared googlesheet,", class = "main-steps-title"),
-    textInput(ns("url"),
-              label= NULL,
-              value = "", 
-              width = NULL, 
-              placeholder = "https://docs.google.com/spreadsheets/d/.../edit?usp=sharing"
-    ),
-    h5("then upload the infosheet into tenzing:", class = "main-steps-title"),
-    div(class = "out-btn",
-        actionButton(inputId = ns("submit"),
-                     label = "Load and validate")
-    ),
-  )
+    div(style = "width: 95%; display: inline-block;",
+        fileInput(NS(id, "file"),
+                  label = NULL,
+                  accept = c(
+                    '.csv',
+                    '.tsv',
+                    '.xlsx'),
+                  multiple = FALSE)
+        ),
+    div(style = "width: 5%; display: inline-block; float: right;",
+        title = "Click to upload from file",
+        actionButton(NS(id, "upload_file"),
+                     label = NULL,
+                     icon = icon("upload", lib = "font-awesome"),
+                     class = "upload-btn")
+        ),
+    h5("or paste the url of a shared googlesheet", class = "main-steps-title"),
+    div(style = "width: 95%; display: inline-block;",
+        textInput(NS(id, "url"),
+                  label= NULL,
+                  value = "", 
+                  width = NULL, 
+                  placeholder = "https://docs.google.com/spreadsheets/d/.../edit?usp=sharing")
+        ),
+    div(style = "width: 5%; display: inline-block; float: right;",
+        title = "Click to upload from url",
+        actionButton(NS(id, "upload_url"),
+                     label = NULL,
+                     icon = icon("upload", lib = "font-awesome"),
+                     class = "upload-btn")
+        )
+    )
+    
 }
     
 # Module Server
@@ -46,45 +59,75 @@ mod_read_spreadsheet_ui <- function(id){
 #' @keywords internal
     
 mod_read_spreadsheet_server <- function(id) {
-  # File uploading limit: 9MB.
+  # File uploading limit: 9MB
   options(shiny.maxRequestSize = 9*1024^2)
   
   moduleServer(id, function(input, output, session) {
-    # Reading infosheet from local
+    # Create one activate reactive from two buttons
+    # If either of the buttons are pressed the reactive fires
+    activate <- reactive(
+      if (input$upload_url == 0 & input$upload_file == 0) {
+        NULL
+      } else {
+        input$upload_url + input$upload_file
+      }
+    )
     
+    # Decide which input to read
+    # The app reads the input of the latest upload button is pressed
+    which_input <- reactiveVal(NULL)
     
-    table_data <- eventReactive(input$submit, {
-      # File input requirement
-      #req(input$file)
-      infosheet_path = ifelse(is.null(input$file),input$url,input$file$datapath)
-      
+    observe({
+      req(input$upload_file)
+      which_input("file")
+    })
+    
+    observe({
+      req(input$upload_url)
+      which_input("url")
+    })
+    
+    # Reading infosheet
+    table_data <- eventReactive(activate(), {
+      # Googlesheets authentication
       googlesheets4::gs4_deauth()
+      
+      # Select the path to read based on which action button is pressed
+      if (which_input() == "file") {
+        if (is.null(input$file)) {
+          infosheet_path <- ""
+          } else {
+            infosheet_path <- input$file$datapath
+            }
+        } else if (which_input() == "url") {
+          infosheet_path <- input$url
+          }
+      
+      # Reading data
       read_infosheet(infosheet_path = infosheet_path)
       })
-
-    
-    
-    
     
     # Alert modal if infosheet is incomplete
-    #valid_infosheet <- mod_check_modal_server("check_modal_ui_1", activate = reactive(input$file), table_data = table_data)
-    
-    valid_infosheet <- mod_check_modal_server("check_modal_ui_1", activate = reactive(input$submit), table_data = table_data)
-    
+    valid_infosheet <- mod_check_modal_server("check_modal_ui_1", activate = activate, table_data = table_data)
+
     # Delete empty rows
-    table_data_clean <- eventReactive(valid_infosheet, {
+    table_data_clean <- eventReactive(activate(), {
+      if (valid_infosheet() == TRUE) {
       table_data() %>%
       tibble::as_tibble() %>%
       dplyr::filter_at(
         dplyr::vars(Firstname, `Middle name`, Surname),
         dplyr::any_vars(!is.na(.)))
+      } else {
+        NULL
+      }
     })
     
     # Return module output
     return(list(
       data = table_data_clean,
       valid_infosheet = valid_infosheet,
-      uploaded = reactive(input$submit)
+      uploaded = activate
       ))
   })
 }
