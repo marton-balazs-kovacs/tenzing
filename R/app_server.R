da94804020c6e18be08f24772c135c2277da100c
Marton Kovacs <marton.balazs.kovacs@gmail.com>
2025-10-29 12:42:05 +0100

    factored out support popup

--- R/app_server.R
+++ R/app_server.R
@@ -1,51 +1,8 @@
 #' @import shiny
 #' @import shinyjs
-app_server <- function(input, output,session) {
-  show_popup = FALSE
-  observe({
-  if (is.null(session$userData$app_open_count)) {
-    session$userData$app_open_count <- shiny::reactiveVal(0)
-    
-    #Calculate whether should create the pop-up, by using the date/time as a random number proxy
-    # Get the current time
-    current_time <- Sys.time()
-    # Format the time as a continuous string
-    formatted_time <- format(current_time, "%H%M%S")
-    # Convert the string to a numeric value
-    numeric_time <- as.numeric(formatted_time)
-    if ((numeric_time %% 3) == 0) { #modulus two seems to make it show up less than 50% of time?
-      show_popup = TRUE
-    }
-    
-  }
-  
-if (show_popup) {
-    # Show a non-aggressive pop-up notification using shinyjs
-    # To get a random number to decide ask_for_citation_not_donation, use part of the date
-    current_date <- Sys.time()
-    formatted_date <- format(current_date, "%S%Y%m%d")
-    numeric_date <- as.numeric(formatted_date)
-    if ((numeric_date %% 3) == 0) { #modulus two seems to make it show up less than 50% of time?
-      msg <-"Please support tenzing 😊 by <br>donating <a href='https://opencollective.com/tenzing' target='_blank'>here</a>."
-    } else {
-      msg <-"Please cite tenzing (references listed at bottom😊)"
-    }      
-    shinyjs::runjs(glue("const div = document.createElement('div');
-                           div.innerHTML = '{msg}';
-                           div.style.position = 'fixed';
-                           div.style.top = '75%';
-                           div.style.left = '16%';
-                           div.style.padding = '10px';
-                           div.style.background = 'lightblue';
-                           div.style.border = '1px solid gray';
-                           div.style.borderRadius = '5px';
-                           div.style.zIndex = 9999;
-                           document.body.appendChild(div);
-                           // Remove the notification after 60 seconds
-                           setTimeout(function() {{ div.remove(); }}, 60000);
-                        "))
-          }
- })
+app_server <- function(input, output, session) {
+  # Popup for support
+  mod_support_popup_server("support_popup")
   
   # Read in the contributors_table
   ## Save the read data as a reactive object
---
+++ R/mod_support_popup.R
@@ -0,0 +1,79 @@
+#' Support popup (server-only; no UI function)
+#' Uses insertUI/removeUI + later; styled via CSS (.support-toast)
+mod_support_popup_server <- function(
+    id,
+    enable       = TRUE,
+    show_prob    = 0.33,
+    delay_ms     = 1500,
+    dismiss_ms   = 60000,
+    donation_url = "https://opencollective.com/tenzing"
+) {
+  shiny::moduleServer(id, function(input, output, session) {
+    ns <- session$ns
+
+    is_enabled <- function() {
+      if (isTRUE(is.reactive(enable))) isTRUE(enable()) else isTRUE(enable)
+    }
+    
+    guard_key <- "tenzing_support_popup_shown"
+    if (is.null(session$userData[[guard_key]])) {
+      session$userData[[guard_key]] <- shiny::reactiveVal(FALSE)
+    }
+    
+    if (is.null(session$userData$tenzing_support_popup_coin)) {
+      p <- suppressWarnings(as.numeric(show_prob))
+      p <- if (is.na(p) || p < 0 || p > 1) 0 else p
+      session$userData$tenzing_support_popup_coin <- stats::runif(1) < p
+    }
+    
+    build_message <- function() {
+      ask_donation <- (as.integer(format(Sys.Date(), "%j")) %% 2) == 0
+      if (ask_donation) {
+        glue::glue(
+          "Please support tenzing 😊 by <br>donating ",
+          "<a href='{donation_url}' target='_blank' rel='noopener noreferrer'>here</a>."
+        )
+      } else {
+        "Please cite tenzing (references listed at bottom 😊)"
+      }
+    }
+    
+    shiny::observe({
+      if (!is_enabled()) return()
+      if (isTRUE(session$userData[[guard_key]]())) return()
+      if (!isTRUE(session$userData$tenzing_support_popup_coin)) return()
+      
+      msg <- build_message()
+      delay_s   <- max(0, as.integer(delay_ms))   / 1000
+      dismiss_s <- max(0, as.integer(dismiss_ms)) / 1000
+      toast_id  <- ns("support_toast")
+      
+      later::later(function() {
+        shiny::insertUI(
+          selector = "body",
+          where    = "beforeEnd",
+          immediate = TRUE,
+          session  = session,         # <-- important
+          ui = shiny::div(
+            id = toast_id,
+            class = "support-toast",
+            shiny::HTML(msg)
+          )
+        )
+        
+        if (dismiss_s > 0) {
+          later::later(function() {
+            shiny::removeUI(
+              selector  = paste0("#", toast_id),
+              multiple  = FALSE,
+              immediate = TRUE,
+              session   = session      # <-- important
+            )
+          }, delay = dismiss_s)
+        }
+      }, delay = delay_s)
+      
+      session$userData[[guard_key]](TRUE)
+    })
+  })
+}
